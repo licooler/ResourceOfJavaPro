2df3f9af0cc1b990fe2e2d5bafc095f38b6c34a8
xiaoliangwang <76183185@qq.com>
2015-03-05 16:38:01 +0800

    testFileIO20150305

--- src/com/wxl/fileStu/test.java
+++ src/com/wxl/fileStu/test.java
@@ -1,5 +1,6 @@
 package com.wxl.fileStu;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -7,20 +8,42 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class test {
-	private static String path = "";
+	private static String staticPath = "staticFiles";
+	private static String fileName = "FileIO.txt";
 	public static void main(String[] args) {
 		
-		InputStream in;
 		try {
-			File file = new File(path);
-			in = new FileInputStream(file);
-			int temp;
-			while((temp=in.read()) != -1){
-				System.out.println(temp);
-			}
+			File file = new File("");
 			
+			String path = file.getCanonicalPath() + File.separator 
+					+ staticPath + File.separator + fileName;
+			
+			InputStream in = new FileInputStream(new File(path));
+			int temp;
+			System.out.println("input stream!");
+			while((temp=in.read()) != -1){
+				System.out.print((char)temp);
+				System.out.print("(" + temp + ") ");
+			}
+			System.out.print("\n");
+			
+			in = new FileInputStream(new File(path));
 			InputStreamReader re = new InputStreamReader(in);
-		
+			System.out.println("input stream reader!");
+			while((temp = re.read()) != -1){
+				System.out.print((char)temp);
+				System.out.print("(" + temp + ") ");
+			}
+			System.out.println("");
+			
+			in = new FileInputStream(new File(path));
+			re = new InputStreamReader(in);
+			BufferedReader bf = new BufferedReader(re);
+			System.out.println("buffered reader!");
+			String txt = "";
+			while((txt = bf.readLine()) != null){
+				System.out.println(txt);
+			}
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
