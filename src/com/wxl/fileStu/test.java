1827f03d1545383e20d3a6d62d52f2aa04f029a1
xiaoliangwang <76183185@qq.com>
2015-03-05 15:09:32 +0800

    file study 20150305

--- src/com/wxl/fileStu/test.java
+++ src/com/wxl/fileStu/test.java
@@ -1,7 +1,27 @@
 package com.wxl.fileStu;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class test {
+	private static String path = "";
 	public static void main(String[] args) {
+		
+		InputStream in;
+		try {
+			File file = new File(path);
+			in = new FileInputStream(file);
+			int temp;
+			while((temp=in.read()) != -1){
+				System.out.println(temp);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 }
